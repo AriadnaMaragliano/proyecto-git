a24ea7807e9791b1d343a76b9003b522e5bf56c2
AriadnaMaragliano <90161116+AriadnaMaragliano@users.noreply.github.com>
2021-09-07 23:47:22 -0300

    ProyectoV0.1

---
+++ Proyecto Git.py
@@ -0,0 +1,8 @@
+#Crear un programa donde se permita la compra de productos.
+#Al iniciar el mismo, se le solicitará al usuario que ingrese el código de producto a adquirir.
+#En el sistema existirán 3 tipos de productos diferentes,por compra solo se podrá levar un tipo de producto.#
+#Una vez ingresado el producto a llevar,el sistema deberá solicitar la cantidad a comprar.
+#Se podra realizar la compra de un maximo de 3 productos.#
+#Si se compran 2 productos se aplicará un descuento del 50% en la segunda unidad.
+#Si se compran 3 productos se abonarán 2.
+#Finalmente, debe mostrar el total a pagar en funcion al precio del producto y el descuento aplicado
